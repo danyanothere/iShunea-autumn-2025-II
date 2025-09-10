95a2d5b474f1d1df5d4ba1019b3ef240aed96739
danyanothere <2gdanyd@gmail.com>
2025-09-10 04:00:00 +0300

    Add files via upload
    feat(admin): setup env, clerk auth & modal components

---
+++ (root)/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () =>{
+  const { onOpen, isOpen } = useStoreModal();
+
+  useEffect (() => {
+if (!isOpen) {
+  onOpen();
+}
+  }, [isOpen, onOpen] );
+
+
+  return (
+   <div className="p-4">
+   Root Page
+   </div>
+  );
+}
+
+
+export default SetupPage;
