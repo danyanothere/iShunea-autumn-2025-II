d6630d661ebf309ac9301948b5382ed622bf26ff
danyanothere <2gdanyd@gmail.com>
2025-09-11 13:05:47 +0300

    farm components; prisma supabase mysql; dashboard

--- layout.tsx
+++ layout.tsx
@@ -1,11 +1,35 @@
-export default function AuthLayout ({
-    children
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+    children,
+    params
 }: {
-    children: React.ReactNode
+    children: React.ReactNode;
+    params: Promise<{ storeId: string }>
 }) {
+    const { userId } = await auth(); 
+    const { storeId } = await params;
+    
+    if (!userId) {
+        redirect('/sign-in');
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: storeId,
+            userId
+        }
+    });
+
+    if (!store) {
+        redirect('/');
+    }
     return (
-        <div className="flex items-center justify-center h-full">
-          {children}
-           </div>
+        <>
+        <div>This will be a Navbar</div>
+        {children}
+        </>
     )
 }
--- page.tsx
+++ page.tsx
@@ -1,5 +1,24 @@
-import { SignIn } from '@clerk/nextjs'
+import prismadb from "@/lib/prismadb";
 
-export default function Page() {
-  return <SignIn />
+interface DashboardPageProps {
+    params: {storeId: string}
+};
+
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({
+    params
+}) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    });
+
+    return (
+        <div>
+            Active store: {store?.name}
+        </div>
+    );
 }
+
+export default DashboardPage;
